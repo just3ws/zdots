9103f42423d271ef988cd4dcfd7773deb8c63c9b
Mike Hall <mike@benchprep.com>
2017-05-30 08:38:39 -0500

    Removed dead setup, only for iTerm2

--- bin/setup.zsh
+++ bin/setup.zsh
@@ -1,8 +1,2 @@
-mkdir -p ~/.config/zsh
-mkdir -p ~/.local/share/zsh/cache
-
-mv -f ~/.zshenv ~/.zshenv.bak
-cp -f ./template/.zshenv ~/.zshenv
-
 npm install --global imgcat
 npm install --global imgls
