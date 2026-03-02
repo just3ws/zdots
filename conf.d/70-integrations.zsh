bf8686e340aac56a73834e97ac5d9d74986e95c7
Mike Hall <mike@just3ws.com>
2026-03-02 09:55:58 -0600

    Fix FZF history keybindings and load custom fzfrc

--- conf.d/70-integrations.zsh
+++ conf.d/70-integrations.zsh
@@ -8,5 +8,25 @@ if [[ -o interactive && -t 1 && -r "$ZDOTDIR/.fzf.zsh" ]]; then
   source "$ZDOTDIR/.fzf.zsh"
 fi
 
+if [[ -o interactive && -t 1 && -r "$ZDOTDIR/fzfrc" ]]; then
+  source "$ZDOTDIR/fzfrc"
+fi
+
 # Machine-local and private overrides.
 [[ -r "$ZDOTDIR/.zshrc.local" ]] && source "$ZDOTDIR/.zshrc.local"
+
+# Keep ^R deterministic: prefer fzf history when available, otherwise use
+# built-in incremental history search across keymaps.
+if [[ -o interactive ]]; then
+  if (( ${+widgets[fzf-history-widget]} )); then
+    bindkey '^R' fzf-history-widget
+    bindkey -M emacs '^R' fzf-history-widget
+    bindkey -M viins '^R' fzf-history-widget
+    bindkey -M vicmd '^R' fzf-history-widget
+  else
+    bindkey '^R' history-incremental-search-backward
+    bindkey -M emacs '^R' history-incremental-search-backward
+    bindkey -M viins '^R' history-incremental-search-backward
+    bindkey -M vicmd '^R' history-incremental-search-backward
+  fi
+fi
